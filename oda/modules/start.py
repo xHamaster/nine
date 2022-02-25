607f5bad6dfa29c0f073e93fe10baeba32f138dc
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-25 20:49:38 +0530

    Update start.py

--- oda/modules/start.py
+++ oda/modules/start.py
@@ -8,6 +8,21 @@ from oda.utils.filters import command
 
 from oda.config import BOT_USERNAME
 
+@Client.on_callback_query(filters.regex("cbsetting"))
+async def cbsetting(_, query: CallbackQuery):
+    await query.edit_message_text(
+        f"""**ᴘᴀᴠᴀɴ ꜱᴇᴛᴛɪɴɢꜱ ᴄᴏᴍᴍᴀɴᴅꜱ :**
+» /pause - ꜰᴏʀ ᴘᴀᴜꜱɪɴɢ ꜱᴛʀᴇᴀᴍɪɴɢ. 
+» /resume - ꜰᴏʀ ʀᴇꜱᴜᴍᴇ ꜱᴛʀᴇᴀᴍɪɴɢ. 
+» /skip - ꜰᴏʀ ꜱᴋɪᴘᴘɪɴɢ ᴄᴜʀʀᴇɴᴛ ꜱᴏɴɢ ᴀɴᴅ ᴘʟᴀʏɪɴɢ ɴᴇxᴛ ꜱᴏɴɢ. 
+» /mute - ꜰᴏʀ ᴍᴜᴛᴜɪɴɢ ᴀꜱꜱɪꜱᴛᴀɴᴛ ɪɴ ᴠᴄ. 
+» /unmute - ꜰᴏʀ ᴜɴᴍᴜᴛᴇ ᴀꜱꜱɪꜱᴛᴀɴᴛ ɪɴ ᴠᴄ. 
+» /end - ꜰᴏʀ ᴇɴᴅ ꜱᴛʀᴇᴀᴍɪɴɢ. 
+**© @TheCreatorPavan**""",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔙  ʙᴀᴄᴋ ᴛᴏ ʜᴏᴍᴇ", callback_data="cbcmds")]]
+        ),
+    )
 
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
@@ -18,7 +33,7 @@ async def start_(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", url=f"https://t.me/adityaserver")
+                        "➕ ❰ ᴊᴏɪɴ ʜᴇʀᴇ ғᴏʀ ᴜᴘᴅᴀᴛᴇs ❱ ➕", callback_data="cbsetting")
                 ]
                 
            ]
