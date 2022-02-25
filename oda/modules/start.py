f7e56c4d2fa316e77e575f026db1ff2ee6a19ef1
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-25 20:05:07 +0530

    Create start.py

--- oda/modules/start.py
+++ oda/modules/start.py
@@ -9,6 +9,15 @@ from oda.utils.filters import command
 
 from oda.config import BOT_USERNAME
 
+@Client.on_callback_query(filters.regex(pattern=r"^(cls)$"))
+async def closed(_, query: CallbackQuery):
+    from_user = query.from_user
+        return await query.answer(
+            "You don't have enough permissions to perform this action.\n"
+            + f"Hey Permission",
+            show_alert=True,
+        )
+
 @Client.on_message(
     command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
 )
@@ -18,19 +27,19 @@ async def start_(c: Client, message: Message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("𝖢𝗈𝗆𝗆𝖺𝗇𝖽𝗌", callback_data="command_list"),
+                    InlineKeyboardButton("Commands", callback_data="cls"),
                 ],
                 [
                     InlineKeyboardButton(
-                        "𝖲𝗎𝗉𝗉𝗈𝗋𝗍 𝖦𝗋𝗈𝗎𝗉", url=f"https://t.me/CreatorPavanChat"
+                        "Official Group", url=f"https://t.me/CreatorPavanChat"
                     ),
                     InlineKeyboardButton(
-                        "𝖴𝗉𝖽𝖺𝗍𝖾𝗌 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", url=f"https://t.me/creatorpavan"
+                        "Official Channel", url=f"https://t.me/creatorpavan"
                     ),
                 ],
                 [
                     InlineKeyboardButton(
-                        "𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉",
+                        "➕ Add me to your Group ➕",
                         url=f"https://t.me/RessoMusicBot?startgroup=true"
                     )
                 ],
