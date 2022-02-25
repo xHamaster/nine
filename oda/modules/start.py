310ff7ac414fb27f35e2c00f260cc7091c89c3b7
ALONE-OP-ALONE <98104781+ALONE-OP-ALONE@users.noreply.github.com>
2022-02-25 19:11:10 +0530

    Update start.py

--- oda/modules/start.py
+++ oda/modules/start.py
@@ -1,1 +1,45 @@
 
+import asyncio
+
+from datetime import datetime
+from sys import version_info
+from time import time
+from pyrogram import Client, filters, __version__ as pyrover
+from pyrogram.errors import FloodWait
+from pytgcalls import (__version__ as pytover)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
+
+
+from config import (
+    BOT_USERNAME,
+
+@Client.on_message(
+    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
+)
+async def start_(c: Client, message: Message):
+    await message.reply_text(
+        f"""💖 **Welcome {message.from_user.mention()}""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Add Me To Your Group",
+                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+                    )
+                ],
+                [InlineKeyboardButton("About", callback_data="user_guide")],
+                [
+                    InlineKeyboardButton("Commands", callback_data="command_list"),
+                ],
+                [
+                    InlineKeyboardButton(
+                        "Support Group", url=f"https://t.me/CreatorPavanChat}"
+                    ),
+                    InlineKeyboardButton(
+                        "Updates Channel", url=f"https://t.me/creatorpavan"
+                    )
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
