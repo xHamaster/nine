892c29e9c2b5ce66efb3182f23629e7457a45403
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-26 08:11:29 +0530

    Update play.py

--- oda/modules/play.py
+++ oda/modules/play.py
@@ -123,6 +123,36 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     os.remove("temp.png")
     os.remove("background.png")
 
+
+# Change image size
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    return image.resize((newWidth, newHeight))
+
+
+async def generate_cover(requested_by, title, views, duration, thumbnail):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("background.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+
+    image1 = Image.open("./background.png")
+    image2 = Image.open("etc/foreground.png")
+    image3 = changeImageSize(1024, 1024, image1)
+    image4 = changeImageSize(1024, 1024, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save("temp.png")
+    img = Image.open("temp.png")
+    img.save("final.png")
+    os.remove("temp.png")
+    os.remove("background.png")
+
 def others_markup(videoid, user_id):
     buttons = [
         [
