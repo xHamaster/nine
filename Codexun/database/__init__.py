7f9e597f757a34b538e1a678875bd9476e0476c1
ALONE-OP-ALONE <98104781+ALONE-OP-ALONE@users.noreply.github.com>
2022-02-27 10:06:18 +0530

    Update __init__.py

--- Codexun/database/__init__.py
+++ Codexun/database/__init__.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from oda.config import MONGO_DB_URI
+from Codexun.config import MONGO_DB_URI
 
 
 MONGODB_CLI = MongoClient(MONGO_DB_URI)
