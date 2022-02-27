33907c175bbe404d43f48a1d548951b7ee306019
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-27 13:18:19 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -120,20 +120,20 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     Image.alpha_composite(image5, image6).save("temp.png")
     img = Image.open("temp.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("etc/Biryani-Black.ttf", 65)
+    font = ImageFont.truetype("etc/Codexun.otf", 65)
     draw.text((25, 250),
         f"{title}..",
         (255, 255, 255),
         font=font,
     )
-    font = ImageFont.truetype("etc/Biryani-Black.ttf", 50)
-    draw.text((25, 300),
+    font = ImageFont.truetype("etc/Codexun.otf", 40)
+    draw.text((25, 450),
         f"Views: {views}",
         (255, 255, 255),
         font=font,
     )
-    font = ImageFont.truetype("etc/Biryani-Black.ttf", 50)
-    draw.text((25, 340),
+    font = ImageFont.truetype("etc/Codexun.otf", 40)
+    draw.text((25, 500),
         f"Duration: {duration} min.",
         (255, 255, 255),
         font=font,
