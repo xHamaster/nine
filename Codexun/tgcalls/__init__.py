6264dc535de939b6fc3a18b637716afc9d7daa8f
ALONE-OP-ALONE <98104781+ALONE-OP-ALONE@users.noreply.github.com>
2022-02-27 10:11:24 +0530

    Update __init__.py

--- Codexun/tgcalls/__init__.py
+++ Codexun/tgcalls/__init__.py
@@ -1,13 +1,13 @@
 from os import listdir, mkdir
 from pyrogram import Client
-from oda import config
-from oda.tgcalls.queues import clear, get, is_empty, put, task_done
-from oda.tgcalls import queues
-from oda.tgcalls.youtube import download
-from oda.tgcalls.calls import run, pytgcalls
-from oda.tgcalls.calls import client
+from Codexun import config
+from Codexun.tgcalls.queues import clear, get, is_empty, put, task_done
+from Codexun.tgcalls import queues
+from Codexun.tgcalls.youtube import download
+from Codexun.tgcalls.calls import run, pytgcalls
+from Codexun.tgcalls.calls import client
 
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from oda.tgcalls.convert import convert
+from Codexun.tgcalls.convert import convert
