0f4292dc1385d7dab49d1c84e37aa5cf7b7daed1
ALONE-OP-ALONE <98104781+ALONE-OP-ALONE@users.noreply.github.com>
2022-02-27 10:08:47 +0530

    Update decorators.py

--- Codexun/utils/decorators.py
+++ Codexun/utils/decorators.py
@@ -3,7 +3,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from oda.config import SUDO_USERS
+from Codexun.config import SUDO_USERS
 
 
 def errors(func: Callable) -> Callable:
