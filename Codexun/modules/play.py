874aabe6087f21235cb2e8c283415a6451cc8593
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-28 22:23:55 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -140,9 +140,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     font4 = ImageFont.truetype(r'Utils/arial_bold.ttf', 35)
 
     image4 = ImageDraw.Draw(image2)
-    image4.text((10, 10), bot, fill="white", font = font1, align ="left") 
-    image4.text((670, 150), status, fill="white", font = font2, align ="left") 
-
+    
     # title
     title1 = truncate(title)
     image4.text((670, 300), text=title1[0], fill="white", font = font3, align ="left") 
