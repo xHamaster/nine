c68cb872b5623fc0078d02363f6bd5f7108882cf
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-02-28 23:26:31 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -129,7 +129,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
     image3 = image3.resize((600,600))
 
-    image2.paste(circle, (50,70), mask = circle)
+    image2.paste(image3, (50,70), mask = image3)
     image2.paste(circle, (0,0), mask = circle)
 
     image4 = ImageDraw.Draw(image2)
