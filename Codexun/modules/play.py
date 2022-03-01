7d52f46cd09b610cfdf52f72e22303c03dde5a7a
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-01 13:02:53 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -141,7 +141,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     image3 = image.crop((280,0,1000,720))
     lum_img = Image.new('L', [720,720] , 0)
     draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(800,800), (800,800)], 0, 360, fill = 255, outline = "white")
+    draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
     img_arr =np.array(image3)
     lum_img_arr =np.array(lum_img)
     final_img_arr = np.dstack((img_arr,lum_img_arr))
