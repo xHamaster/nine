79db9be11308de8c328d2adab30c4303f22b76a5
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-01 17:32:45 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -98,19 +98,6 @@ def time_to_seconds(time):
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
-def truncate(text):
-    list = text.split(" ")
-    text1 = ""
-    text2 = ""    
-    for i in list:
-        if len(text1) + len(i) < 24:        
-            text1 += " " + i
-        elif len(text2) + len(i) < 24:        
-            text2 += " " + i
-
-    text1 = text1.strip()
-    text2 = text2.strip()     
-    return [text1,text2]
 
 # Change image size
 def changeImageSize(maxWidth, maxHeight, image):
@@ -130,50 +117,14 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.write(await resp.read())
                 await f.close()
 
-    image = Image.open(f"etc/foreground.png")
+    image = Image.open(f"background.png")
     black = Image.open("Utils/black.jpg")
     circle = Image.open("Utils/circle.png")
     image1 = changeImageSize(1280, 720, image)
     image1 = image1.filter(ImageFilter.BoxBlur(20))
     image2 = Image.blend(image1, black, 0.6)
     
-    # Cropping circle from thubnail
-    image3 = image.crop((280,0,720,720))
-    lum_img = Image.new('L', [720,720] , 0)
-    draw = ImageDraw.Draw(lum_img)
-    draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
-    img_arr =np.array(image3)
-    lum_img_arr =np.array(lum_img)
-    final_img_arr = np.dstack((img_arr,lum_img_arr))
-    image3 = Image.fromarray(final_img_arr)
-    image3 = image3.resize((600,600))
-
-    image2.paste(image3, (0,0), mask = image3)
-    image2.paste(circle, (0,0), mask = circle)
-
     
-    # fonts
-    font1 = ImageFont.truetype(r'Utils/arial_bold.ttf', 30)
-    font2 = ImageFont.truetype(r'Utils/arial_black.ttf', 60)
-    font3 = ImageFont.truetype(r'Utils/arial_black.ttf', 40)
-    font4 = ImageFont.truetype(r'Utils/arial_bold.ttf', 35)
-
-    image4 = ImageDraw.Draw(image2)
-
-    # title
-    title1 = truncate(title)
-    image4.text((670, 300), text=title1[0], fill="white", font = font3, align ="left") 
-    image4.text((670, 350), text=title1[1], fill="white", font = font3, align ="left") 
-
-    # description
-    views = f"Views : {views}"
-    duration = f"Duration : {duration} Mins"
-
-    image4.text((670, 450), text=views, fill="white", font = font4, align ="left") 
-    image4.text((670, 500), text=duration, fill="white", font = font4, align ="left") 
-
-
-
     image2.save(f"final.png")
     os.remove(f"background.png")
     final = f"temp.png"
