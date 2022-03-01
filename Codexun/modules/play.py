4caab573bbc7d8f1bbd0d7e42d9546d2e3443e7e
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-01 08:50:58 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -130,7 +130,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.write(await resp.read())
                 await f.close()
 
-    image = Image.open(f""etc/foreground.png"")
+    image = Image.open(f"etc/foreground.png")
     black = Image.open("Utils/black.jpg")
     circle = Image.open("Utils/circle.png")
     image1 = changeImageSize(1280, 720, image)
