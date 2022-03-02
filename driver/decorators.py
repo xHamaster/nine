bc312f6be27ff79dba247c2debbe2c119645560f
ALONE-OP-ALONE <98104781+ALONE-OP-ALONE@users.noreply.github.com>
2022-03-02 07:00:29 +0530

    Update decorators.py

--- driver/decorators.py
+++ driver/decorators.py
@@ -1,7 +1,7 @@
 from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
-from config import SUDO_USERS
+from Codexun.config import SUDO_USERS
 from driver.admins import get_administrators
 
 
