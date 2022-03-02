a4b7b4c9760091fa5b53c2f9e53d8dfeeabe4797
ALONE-OP-ALONE <98104781+ALONE-OP-ALONE@users.noreply.github.com>
2022-03-02 08:05:27 +0530

    Update thumbnail.py

--- driver/design/thumbnail.py
+++ driver/design/thumbnail.py
@@ -17,7 +17,7 @@ async def thumb(thumbnail, title, userid, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
+                f = await aiofiles.open(f"driver/source/20220227_215805.jpg", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
     image1 = Image.open(f"driver/source/20220227_215805.jpg")
