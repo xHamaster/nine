f7da75f9ca3c261667eb3a8b3a5d72aaaba6e79b
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-03 14:35:00 +0530

    Update usefull.py

--- Codexun/modules/usefull.py
+++ Codexun/modules/usefull.py
@@ -51,48 +51,6 @@ async def telegraph(client, message):
 
 # ====== TELEGRAPH ======
 
-@Client.on_message(
-    filters.command("purge") &
-    admin_fliter
-)
-async def purge(client, message):
-    """ purge upto the replied message """
-    if message.chat.type not in (("supergroup", "channel")):
-        # https://t.me/c/1312712379/84174
-        return
-
-    status_message = await message.reply_text("Processing..", quote=True)
-    await message.delete()
-    message_ids = []
-    count_del_etion_s = 0
-
-    if message.reply_to_message:
-        for a_s_message_id in range(
-            message.reply_to_message.message_id,
-            message.message_id
-        ):
-            message_ids.append(a_s_message_id)
-            if len(message_ids) == TG_MAX_SELECT_LEN:
-                await client.delete_messages(
-                    chat_id=message.chat.id,
-                    message_ids=message_ids,
-                    revoke=True
-                )
-                count_del_etion_s += len(message_ids)
-                message_ids = []
-        if len(message_ids) > 0:
-            await client.delete_messages(
-                chat_id=message.chat.id,
-                message_ids=message_ids,
-                revoke=True
-            )
-            count_del_etion_s += len(message_ids)
-
-    await status_message.edit_text(
-        f"deleted {count_del_etion_s} messages"
-    )
-    await asyncio.sleep(5)
-    await status_message.delete()
 
 
 @Client.on_message(
