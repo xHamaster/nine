93243f56a667d32927552dab0f8da5677b5d6aa3
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-05 23:34:02 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -32,7 +32,8 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     CallbackQuery,
 )
-from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, ChatWriteForbidden
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from Codexun.tgcalls import calls, queues
 from Codexun.tgcalls.calls import client as ASS_ACC
@@ -206,6 +207,35 @@ menu_keyboard = InlineKeyboardMarkup(
         ],
     ]
 )
+
+
+@Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MUST_JOIN:  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    disable_web_page_preview=True,
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("✨ Join Channel ✨", url="https://t.me/Codexun")]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+
 
 @Client.on_message(command(["menu", "settings"]) & ~filters.edited)
 async def menu(client: Client, message: Message):
