abfa7f78f77cadf23d3f2e3473d5081e02c17456
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-06 18:55:47 +0530

    Update play.py

--- Codexun/modules/play.py
+++ Codexun/modules/play.py
@@ -211,33 +211,6 @@ menu_keyboard = InlineKeyboardMarkup(
 )
 
 
-@Client.on_message(~filters.edited & filters.incoming & filters.private, group=-1)
-async def must_join_channel(bot: Client, msg: Message):
-    MUST_JOIN = Codexun
-    if not MUST_JOIN:  # Not compulsory
-        return
-    try:
-        try:
-            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
-        except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
-            else:
-                chat_info = await bot.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
-            try:
-                await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url="https://t.me/Codexun")]
-                    ])
-                )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
 
 
 @Client.on_message(command(["menu", "settings"]) & ~filters.edited)
