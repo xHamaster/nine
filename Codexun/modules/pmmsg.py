20ca8f866c46c65f2d501f5fa4e88d1339af1ba2
xHamaster <105044443+xHamaster@users.noreply.github.com>
2022-05-17 01:09:36 +0530

    Update pmmsg.py

--- Codexun/modules/pmmsg.py
+++ Codexun/modules/pmmsg.py
@@ -2,9 +2,11 @@ from pyrogram import Client
 from Codexun.tgcalls import client as USER
 from pyrogram import filters
 from pyrogram.types import Chat, Message, User
-
+from Codexun.config import (
+    BOT_USERNAME,
+)
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"🔰ᴛʜɪs ɪs ᴀssɪsᴛᴀɴᴛ ᴏғ @RessoMusicBot ᴊᴏɪɴ @TheCreatorPavan ғᴏʀ ᴜᴘᴅᴀᴛᴇᴅ🔰")
+  await USER.send_message(message.chat.id,"Hey, this is the assistant of {BOT_USERNAME}\n\n If you want to make your own music bot then contact us at @TeamCodexun.")
   return
